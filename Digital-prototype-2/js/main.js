e27c937a76f10168cd7444c482bcc2411eaef34c
jzy081288 <jzy081288@gmail.com>
2020-02-19 23:29:57 -0500

    new game

--- Digital-prototype-2/js/main.js
+++ Digital-prototype-2/js/main.js
@@ -63,11 +63,11 @@ function createGhost () {
     {
         for (var x = 0; x < 10; x++)
         {
-            var ghost = ghost.create(x * 48, y * 50, 'ghost');
-            ghost.anchor.setTo(0.5, 0.5);
-            ghost.animations.add('fly', [ 0, 1, 2, 3 ], 20, true);
-            ghost.play('fly');
-            ghost.body.moves = false;
+            var ghosts = ghost.create(x * 48, y * 50, 'ghost');
+            ghosts.anchor.setTo(0.5, 0.5);
+            ghosts.animations.add('fly', [ 0, 1, 2, 3 ], 20, true);
+            ghosts.play('fly');
+            ghosts.body.moves = false;
         }
     }
 
@@ -75,7 +75,7 @@ function createGhost () {
     ghost.y = 50;
 
     //  All this does is basically start the invaders moving. Notice we're moving the Group they belong to, rather than the invaders directly.
-    var tween = game.add.tween(ghost).to( { x: 200 }, 2000, Phaser.Easing.Linear.None, true, 0, 1000, true);
+    var tween = game.add.tween(ghosts).to( { x: 200 }, 2000, Phaser.Easing.Linear.None, true, 0, 1000, true);
 
     //  When the tween loops it calls descend
     tween.onLoop.add(descend, this);
