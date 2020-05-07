568754ebef56e16e05025e285368ca0b3fb7bd0b
jzy081288 <jzy081288@gmail.com>
2020-05-07 15:37:26 -0400

    7 finalBackgroundChange

--- Digital-prototype-7/js/EndGame.js
+++ Digital-prototype-7/js/EndGame.js
@@ -4,6 +4,8 @@ GameStates.makeEndGame = function( game, shared ) {
 
 	// var playButton = null;
 	var stateText = null;
+	var background;
+
 	function goToMenu(pointer) {
 
 		//	Ok, the Play Button has been clicked or touched, so let's stop the music (otherwise it'll carry on playing)
@@ -21,9 +23,8 @@ GameStates.makeEndGame = function( game, shared ) {
 		//	We've already preloaded our assets, so let's kick right into the Main Menu itself.
 		//	Here all we're doing is playing some music and adding a picture and button
 		//	Naturally I expect you to do something significantly better :)
-
+			background = game.add.sprite(0, 0, 'preloaderBackground');
 		
-			game.add.sprite(0, 0, 'start');
 			stateText = game.add.text(game.world.centerX,game.world.centerY,'You Win!\nClick to Start again', { font: '40px Arial', fill: '#fff' });
     		stateText.anchor.setTo(0.5, 0.5);
 			game.input.onTap.addOnce(goToMenu, this);
